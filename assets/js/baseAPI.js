feb00578f0af4bf617070cfda40f5335f03f2597
yuanyt <13914779352@163.com>
2021-03-22 14:42:37 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,27 @@
 $.ajaxPrefilter(function (option) {
     option.url = 'http://ajax.frontend.itheima.net' + option.url
     console.log(option.url);
+
+//统一为有权限的接口设置headers请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers= {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //全局统一挂载complete函数
+    option.complte = function (res) {
+          //无论成功还是失败，都会调用complete函数
+        //会拿到返回的responseJSON数据
+        
+            if (res.responseJSON.status !== 0 && res.responseJSON.message === '身份认证失败！') {
+                //强制清空
+                localStorage.removeItem('token')
+                //跳转登陆页面
+                location.href='/login.html'
+                
+            }
+            
+       
+    }
+    
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+    var layer=layui.layer
+    getUserinfo()
+    
+
+    //退出后台主页
+    $('#btnlogout').on('click', function () {
+        
+        layer.confirm('确认是否退出?', {icon: 3, title:'提示'}, function(index){
+            //1.清除本地存储的token
+            localStorage.removeItem('token')
+            //2.跳转到login登陆页面
+            location.href='/login.html'
+            layer.close(index);
+          });
+    })
+
+})
+function getUserinfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //有权限的须在请求头中携带Authorization
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            console.log(res);
+            if (res.status !==0) {
+                return layer.msg('获取资料失败')
+            }
+           
+            renderAvatar(res.data)
+
+        }
+      
+    })
+}
+//渲染用户的头像
+function renderAvatar(user){
+    var name = user.nickname || user.username
+    $('#weluser').html('欢迎&nbsp;&nbsp;' + name)
+    //渲染头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').src(user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first=name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
